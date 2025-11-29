94e42c630c6e86ed4bb7f77bb0c98034b67c7b64
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-11-29 16:03:48 -0600

    fix: Fixed colored scheme for light theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,13 +6,13 @@ interface HeaderProps {
 
 function Header({ page, onPageSelect, onThemeChange }: HeaderProps) {
   const buttonStyle =
-    'cursor-pointer px-8 py-2 rounded-full hover:text-dark-text';
-  const selectedSyle = 'dark:bg-dark-surface dark:text-dark-text';
+    'cursor-pointer px-8 py-2 rounded-full hover:text-light-text dark:hover:text-dark-text';
+  const selectedSyle = 'bg-light-surface text-light-text dark:bg-dark-surface dark:text-dark-text';
 
   return (
     <div className="flex flex-row justify-between items-center">
       <h1 className="font-mono w-30 font-bold">Hugo Juarez</h1>
-      <div className="flex flex-row gap-8 text-dark-text-muted">
+      <div className="flex flex-row gap-8 text-light-text-muted dark:text-dark-text-muted">
         <button
           className={`${buttonStyle} ${page === 'home' ? selectedSyle : ''}`}
           onClick={() => onPageSelect('home')}
@@ -34,7 +34,7 @@ function Header({ page, onPageSelect, onThemeChange }: HeaderProps) {
       </div>
       <div className="w-30 flex flex-row items-center justify-end">
         <button
-          className="w-5 h-5 text-dark-text-muted cursor-pointer hover:text-dark-text"
+          className="w-5 h-5 text-light-text-muted hover:text-light-text dark:text-dark-text-muted cursor-pointer dark:hover:text-dark-text"
           onClick={onThemeChange}
         >
           <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
