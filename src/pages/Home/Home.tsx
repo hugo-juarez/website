61084284492b448f50df4eb2078be9b1e0cbc01f
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-11-29 22:19:54 -0600

    feat: Added home hero section text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
-import Header from "../../components/Header";
+import Header from '../../components/Header';
 
 function Home() {
   return (
     <>
-      <Header page='home'/>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header page="home" />
+      <section className="my-16 flex flex-col gap-4 text-body w-2/5">
+        <h1 className="font-mono text-h1 mb-8">Hi, I'm Hugo</h1>
+        <p>Embedded Software Engineer looking to improve it’s portfolio while building whatever comes to my mind.</p>
+        <p>Into low-level coding and making programms that have an effect on a real life system. </p>
+        <p>I post videos mostly to keep myself on track, but if you like watching someone casually struggle through projects, feel free to follow.</p>
+      </section>
     </>
   );
 }
