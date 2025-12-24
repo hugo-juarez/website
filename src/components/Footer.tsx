4792a4f66b32b839248b9cfa6286beb6b984eb85
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-12-23 22:44:06 -0600

    feat: Added footer with icons

---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+import { motion, useInView } from "motion/react";
+import {
+  FaDiscord,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaSpotify,
+  FaYoutube,
+} from "react-icons/fa";
+import HoverButton from "../animations/HoverButton";
+
+function Footer() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref);
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 20 }}
+      animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+    >
+      <div className="flex flex-col items-center text-light-text-muted dark:text-dark-text-muted gap-4 mt-4 mb-2">
+        <div className="flex flex-row justify-center items-center gap-8 text-h4">
+          <HoverButton>
+            <FaGithub />
+          </HoverButton>
+          <HoverButton>
+            <FaLinkedin />
+          </HoverButton>
+          <HoverButton>
+            <FaDiscord />
+          </HoverButton>
+          <HoverButton>
+            <FaInstagram />
+          </HoverButton>
+          <HoverButton>
+            <FaSpotify />
+          </HoverButton>
+          <HoverButton>
+            <FaYoutube />
+          </HoverButton>
+        </div>
+        <p className="text-caption">@ 2025 Hugo Juarez. All rights reserved.</p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Footer;
