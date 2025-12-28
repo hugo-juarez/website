a4bd31d292dbbd0bb76eb5bb26a55ccf5e6ce746
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-12-28 10:46:54 -0600

    chore: Removed unneccessary div

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,30 +19,29 @@ function Footer() {
       initial={{ opacity: 0, y: 20 }}
       animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
       transition={{ duration: 1, ease: "easeInOut" }}
+      className="flex flex-col items-center text-light-text-muted dark:text-dark-text-muted gap-4 mt-16 mb-2"
     >
-      <div className="flex flex-col items-center text-light-text-muted dark:text-dark-text-muted gap-4 mt-16 mb-2">
-        <div className="flex flex-row justify-center items-center gap-8 text-h4">
-          <HoverButton>
-            <FaGithub />
-          </HoverButton>
-          <HoverButton>
-            <FaLinkedin />
-          </HoverButton>
-          <HoverButton>
-            <FaDiscord />
-          </HoverButton>
-          <HoverButton>
-            <FaInstagram />
-          </HoverButton>
-          <HoverButton>
-            <FaSpotify />
-          </HoverButton>
-          <HoverButton>
-            <FaYoutube />
-          </HoverButton>
-        </div>
-        <p className="text-caption">@ 2025 Hugo Juarez. All rights reserved.</p>
+      <div className="flex flex-row justify-center items-center gap-8 text-h4">
+        <HoverButton>
+          <FaGithub />
+        </HoverButton>
+        <HoverButton>
+          <FaLinkedin />
+        </HoverButton>
+        <HoverButton>
+          <FaDiscord />
+        </HoverButton>
+        <HoverButton>
+          <FaInstagram />
+        </HoverButton>
+        <HoverButton>
+          <FaSpotify />
+        </HoverButton>
+        <HoverButton>
+          <FaYoutube />
+        </HoverButton>
       </div>
+      <p className="text-caption">@ 2025 Hugo Juarez. All rights reserved.</p>
     </motion.div>
   );
 }
