cf8f6432ebf1de9dd78ef464edda75fca70776b3
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-12-28 10:47:45 -0600

    feat: Added motion layout animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,11 @@ import ThemeSelector from "./ThemeSelector";
 import { motion } from "motion/react";
 
 function Header() {
-  const buttonStyle =
-    "cursor-pointer px-8 py-2 rounded-full hover:text-light-text dark:hover:text-dark-text";
-  const selectedSyle =
-    buttonStyle +
-    " " +
-    "bg-light-surface text-light-text dark:bg-dark-surface dark:text-dark-text";
+  const pages = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+  ];
 
   return (
     <div className="flex flex-row justify-between items-center py-2">
@@ -16,27 +15,23 @@ function Header() {
         <img src="/logo.png" className="w-10" />
       </NavLink>
       <nav className="flex flex-row gap-8 text-light-text-muted dark:text-dark-text-muted">
-        <NavLink
-          className={({ isActive }) => (isActive ? selectedSyle : buttonStyle)}
-          to="/"
-          end
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? selectedSyle : buttonStyle)}
-          to="/about"
-          end
-        >
-          About
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? selectedSyle : buttonStyle)}
-          to="/contact"
-          end
-        >
-          Contact
-        </NavLink>
+        {pages.map((page) => (
+          <NavLink
+            to={page.path}
+            className={({isActive}) => `cursor-pointer px-8 py-2 rounded-full hover:text-light-text dark:hover:text-dark-text relative z-0 ${isActive ? "text-light-text dark:text-dark-text" : ""}`}
+            end
+          >
+            {({ isActive }) => (
+              <>
+                {page.label}
+                {isActive && <motion.div 
+                  className="absolute inset-0 rounded-lg -z-10 bg-light-surface dark:bg-dark-surface"
+                  layoutId="selected-page"
+                />}
+              </>
+            )}
+          </NavLink>
+        ))}
       </nav>
       <div className="w-35 flex flex-row items-center justify-end">
         <ThemeSelector />
